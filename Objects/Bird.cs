f0d571d402716272670292bb3e9461d64b11066d
Luka Samkharadze <samkharadzeluke@gmail.com>
2020-10-13 20:13:26 +0400

    Added bird class

---
+++ Objects/Bird.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleFlappyBird.Objects
+{
+    class Bird
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public int Dy { get; set; }
+    }
+}
