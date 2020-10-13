21b443fed0536655a07439fbd590867c913b7510
Luka Samkharadze <samkharadzeluke@gmail.com>
2020-10-13 20:13:32 +0400

    Added tube class

---
+++ Objects/Tube.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleFlappyBird.Objects
+{
+    struct Tube
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public bool[,] Hitbox { get; private set; }
+
+        public Tube(int x_, int y_, int height, int width, int gapHeight)
+        {
+            X = x_;
+            Y = y_;
+
+            Hitbox = new bool[height, width];
+
+            int gapStartY = new Random().Next(1, height - gapHeight);
+
+            for(int y = 0; y < gapStartY && y < height; y++)
+                for(int x = 0; x < width; x++)
+                    Hitbox[y, x] = true;
+
+            for (int y = gapStartY + gapHeight; y < height; y++)
+                for (int x = 0; x < width; x++)
+                    Hitbox[y, x] = true;
+        }
+    }
+}
